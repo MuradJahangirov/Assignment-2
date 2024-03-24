46037cba239c445e1f687996342a1327483d12b7
MuradJahangirov <160223176+MuradJahangirov@users.noreply.github.com>
2024-03-24 20:29:47 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -4,7 +4,21 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+month= str(input('Enter name of the month:'))
+day= int(input('Enter the day:'))
+
+if month in ('January', 'February') or (month == 'December' and day>=21) or\
+   (month == 'March' and day<20):
+    print(f'{month} {day} is in Winter')
+elif month in ('April', 'May') or ( month == 'March' and day>=20) or\
+     (month =='June' and day<21):
+    print(f'{month} {day} is in Spring')
+elif month in ('July', 'August') or (month == 'June' and day>= 21) or\
+     (month == 'September' and day< 22):
+    print(f'{month} {day} is in Summer')
+elif month in ('October', 'November') or (month == 'September' and day>= 22) or\
+     ( month == 'December' and day <21):
+    print(f'{month} {day} is in Fall')
 
 if __name__ == "__main__":
   main()
