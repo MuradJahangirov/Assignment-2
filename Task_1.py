3e14b4a3468863beec47082566956cb640e15b60
MuradJahangirov <160223176+MuradJahangirov@users.noreply.github.com>
2024-03-24 20:32:03 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -2,9 +2,15 @@
 
 def main():
   '''
-  Kodunuzu buraya yazin.
+  letter= input("Enter a letter of the alphabet:")
+if letter in ['A','E','I','O','U','a','e','i','o','u']:
+    print("Entered alphabet is a consonant!")
+elif letter=="Y" or letter=="y":
+    print("Sometimes it is a vowel, and sometimes it is a consonant!")
+else:
+    print("Entered alphabet is a vowel!")
   '''
-  pass
+
 
 if __name__ == "__main__":
   main()
